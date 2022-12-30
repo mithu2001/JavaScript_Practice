1f37b5a2a80404f393c6ad1dc8c6c3016a8f8384
mithu2001 <guptamithu2019@gmail.com>
2022-12-30 21:02:08 +0530

    Brute Force Solution Added

--- 1.sum_zero.js
+++ 1.sum_zero.js
@@ -1,2 +1,18 @@
 //checking Sum Zero problem 1
 //[-5, -4, -3, -2, 0, 2, 4, 6, 8] -> input
+//[-4, 4] -> output
+
+const arr = [-5, -4, -3, -2, 0, 2, 4, 6, 8];
+function getSumPairZero(array){
+    //Brute force Approach
+    for(let num of array){
+        for(let j = 1; j<array.length; j++){
+            if(num + array[j] === 0){
+                return [num, array[j]];
+            }
+        }
+    }
+}
+
+const result = getSumPairZero(arr);
+console.log(result);
