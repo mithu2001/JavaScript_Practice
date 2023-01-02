dc8f3e0f1d7ca36ffa4aa3b9265e4e2fc5b4cab8
mithu2001 <guptamithu2019@gmail.com>
2023-01-02 13:36:22 +0530

    largest Sum

--- LargestSum.js
+++ LargestSum.js
@@ -1,8 +1,28 @@
 
 //[1,2,3,4,3,5,4,6,7,8]
 //count largest sum of consecutive Digits
-function largestSum(){
-
+//num is consecutive number
+function largestSum(array, num){
+    let max = 0;
+    if(num>array){
+        throw new Error("Num is not Greater than Array");
+    }else{
+        
+        for(let i =0; i<array.length- num + 1; i++){
+            let temsum = 0;
+            for(let j = 0; j<num; j++){
+                temsum+=array[i+j];
+                
+            }
+            if(temsum>max){
+                max = temsum;
+            }
+            
+        }
+        
+    }
+    return max;
 }
 
-largestSum();
+const res = largestSum([1,2,3,4,3,5,4,6,7,8],4);
+console.log("Largest Sum is : ",res);
