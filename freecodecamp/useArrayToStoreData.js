377af7f3a7f75500b037e82550dd14dd0ed805c9
mithu2001 <guptamithu2019@gmail.com>
2023-01-09 21:08:46 +0530

    array practice

---
+++ freecodecamp/useArrayToStoreData.js
@@ -0,0 +1,16 @@
+const arr = [
+    "Har Har Mahadev",
+    2023,
+    true,
+    personal_detail = {
+        "Name":"Jeebachh",
+        "Profession":"Software Engineer at Infosys",
+        Qualification :{
+            "B.tech":"Gurukul Kangri University Haridwar",
+            "Branch":"ECE",
+
+        }
+    },
+    
+]
+console.log(arr[3].Qualification.Branch);
